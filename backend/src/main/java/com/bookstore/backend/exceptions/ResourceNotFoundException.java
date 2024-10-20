f326a26d8712115542df5d690f60aa1e5ff1a491
erhanorun <erhanorun@outlook.com>
2024-10-20 14:41:12 +0300

    fix: http response code for post when resource already exists

---
+++ backend/src/main/java/com/bookstore/backend/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.bookstore.backend.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+  public ResourceNotFoundException(String message) {
+    super(message);
+  }
+}
